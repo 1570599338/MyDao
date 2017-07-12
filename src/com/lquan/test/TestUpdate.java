c413166578beba15f2c8fe433aed5720f8854967
lquan <1570599338@qq.com>
2017-07-13 00:12:06 +0800

    执行插入操作

--- src/com/lquan/test/TestUpdate.java
+++ src/com/lquan/test/TestUpdate.java
@@ -28,6 +28,25 @@ public class TestUpdate {
 	}
 	
 	@Test
+	public  void testQueryNoargsInsertData() {
+		
+		String sql = "select pk_id,login_name,dealer_name,role,emailname,login_pwd from t_accounts";
+		List<Account> list = commonDao.queryForPojoList(sql, Account.class);
+		for(Account ac:list){
+			System.out.println("修改前的数据："+ ac.getDealer_name());
+			String sqlinsert ="insert  into t_user(pk_id,log_name,USER_NAME,role,email,password) values(?,?,?,?,?,?) ";
+		
+			int a = commonDao.update(sqlinsert,new Object[]{ac.getPk_id(),ac.getLogin_name(),ac.getDealer_name(),ac.getRole(),ac.getEmailname(),ac.getLogin_pwd()});
+		}
+		
+		List<Account> listx = commonDao.queryForPojoList(sql, Account.class);
+		for(Account u:listx){
+			System.out.println("修改后的数据："+ u.getDealer_name());
+		}
+		
+	}
+	
+	@Test
 	public  void testQueryNoargs() {
 		
 		String sql = "select * from t_accounts where pk_id=1";
@@ -46,10 +65,17 @@ public class TestUpdate {
 			System.out.println("修改后的数据："+ u.getDealer_name());
 		}
 		
-		
-		
-		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
